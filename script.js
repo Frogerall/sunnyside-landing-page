4191c061307967f9506f20671bd39657ab38f4e0
Frogerall <saliticoadrian@gmail.com>
2023-11-22 20:22:41 +0800

    added navigation menu functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+let navBtn = document.querySelector(".nav-menu");
+let navMenu = document.querySelector("nav");
+
+navBtn.addEventListener("click", () => {
+ navMenu.classList.toggle("nav-open");
+});
